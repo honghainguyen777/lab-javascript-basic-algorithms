6166822e3823bdcfe071f5864bfea0ab3ec96db5
honghainguyen777 <n.h.hai2015@gmail.com>
2021-01-11 22:13:58 +0100

    Interation 3 - Hai and Aylin

--- js/index.js
+++ js/index.js
@@ -14,3 +14,22 @@ if (hacker1.length > hacker2.length) {
 }
 
 // Iteration 3: Loops
+let changedNameHacker1 = '';
+
+for (let i = 0; i < hacker1.length; i++) {
+    changedNameHacker1 += hacker1[i].toUpperCase() + ' ';
+} console.log(changedNameHacker1.slice(0, changedNameHacker1.length - 1));
+
+let changedNameHacker2 = '';
+for (let i = hacker2.length - 1; i >= 0; i--) {
+    changedNameHacker2 += hacker2[i]
+} console.log(changedNameHacker2);
+
+let comparedNames = hacker1.localeCompare(hacker2);
+if (comparedNames === -1) {
+    console.log(`The driver's name goes first.`);
+} else if (comparedNames === 1) {
+    console.log(`Yo, the navigator goes first definitely.`);
+} else {
+    console.log(`What?! You both have the same name?`)
+};
